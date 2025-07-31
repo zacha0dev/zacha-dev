f572b648a0a0229ba4ec35eccead28596ace521c
Zachary Allen <zach0dev@gmail.com>
2025-07-31 01:23:12 -0400

    Add Next.js and React dependencies

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // Add other options below
+  // swcMinify: true,
+  // images: { domains: ['example.com'] },
+};
+
+module.exports = nextConfig;
